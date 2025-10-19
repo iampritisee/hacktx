726b30e0c9f4a0a1eb53f1fdba4f833fd4fce5ae
ItsAGamer214 <anish.alle11@gmail.com>
2025-10-19 02:40:13 -0500

    add firebase admin and add sunay shi

--- backend/backend.py
+++ backend/backend.py
@@ -2,24 +2,65 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/dataForConfig', methods = ['GET'])
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+from poc import compute_setup_from_doc
+from health import generate_report
+
+
+cred = credentials.Certificate("../serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+@app.route('/dataForConfig', methods = ['POST'])
 def get_ideal_config():
 
-  data = request.get_json()       # array of jsons for turns and questionnaire response
+  questionnaire = request.get_json()       # questionnaire response only (metadata present in firestore)
 
+  query = db.collection("test_drive").limit(1)
+  results = list(query.stream())
 
-  # result = calc_config(data)      (assume calc_config is computation)
-  # return jsonify(result)       # give back the ideal config
+  metadoc = results[0]
+
+  calc_doc = metadoc.to_dict()
+
+  stability = questionnaire.get('rookie_stability_priority')     # get the preferences
+  steering = questionnaire.get('steering_weight_preference')
+  throttle = questionnaire.get('throttle_pedal_linearity')
+  brake = questionnaire.get('brake_pedal_linearity')
+
+  if 'controls' not in calc_doc['initial_setup']:
+    calc_doc['initial_setup']['controls'] = {}
+
+  # changing preferences in metadoc
+  calc_doc['metadata']['rookie_stability_priority'] = float(stability)
+  calc_doc['initial_setup']['controls']['steering_weight_preference'] = steering
+  calc_doc['initial_setup']['controls']['throttle_pedal_linearity'] = float(throttle)
+  calc_doc['initial_setup']['controls']['brake_pedal_linearity'] = float(brake)
+
+  result = compute_setup_from_doc(calc_doc)     # function from poc
+
+  return jsonify(result.get("optimized_setup", {}))       # return result
+
 
 
 @app.route('/feedback')
 def give_feedback():
 
-  data = request.get_json()       # receive metadata at that turn
+  query = db.collection("race_data").limit(1)      # retrieve race_data
+  results = list(query.stream())
 
-  # result = calc_feedback(data)     # computation for getting feedback
+  data = results[0]
+  
+  converted_data = data.to_dict()
+  result = generate_report(converted_data)      # return health result
 
-  # return jsonify(result)      # give feedback
+  return jsonify(result, 200)
+
 
 
 if __name__ == "__main__":
